276dc27fef4d6819030d7de644e72ef78c9cafbb
Alberto Casanova <58746126+JudahAvadom@users.noreply.github.com>
2022-10-30 18:36:07 -0500

    Context app

--- context/Context.js
+++ context/Context.js
@@ -3,6 +3,10 @@ import { theme } from '../utills'
 
 const GlobalContext = createContext({
     theme,
+    rooms: [],
+    setRooms: () => {},
+    unfilteredRooms: [],
+    setUnfilteredRooms: () => {}
 })
 
 export default GlobalContext;
--- context/ContextWrapper.js
+++ context/ContextWrapper.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Context from './Context';
 import { theme } from '../utills'
 
 export default function ContextWrapper(prop){
+    const [rooms, setRooms] = useState([]);
+    const [unfilteredRooms, setUnfilteredRooms] = useState([]);
     return (
-        <Context.Provider value={{theme}}>
+        <Context.Provider value={{ theme, rooms, setRooms, unfilteredRooms, setUnfilteredRooms }}>
             {prop.children}
         </Context.Provider>
     )
